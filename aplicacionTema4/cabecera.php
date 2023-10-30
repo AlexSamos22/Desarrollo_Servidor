0bad13d11d7a9101231e0ab106e871238984f92e
AlexSamos22 <asammun767@g.educaand.es>
2023-10-30 17:20:08 +0100

    aplicacionTema4

---
+++ aplicacionTema4/cabecera.php
@@ -0,0 +1,7 @@
+<header>
+    Usuario: <?php echo $_SESSION['usuario']['correo']?>
+    <a href="categorias.php">Home</a>
+    <a href="carrito.php">Carrito</a>
+    <a href="logout.php">Cerrar sesión</a>
+</header>
+<hr>
